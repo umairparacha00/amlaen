d56a2fe2c2bff472bedd8083c0c93c534c5afb8a
umairparacha00 <umairparacha0047@gmail.com>
2020-09-01 15:49:13 +0500

    Name_error2

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -311,7 +311,7 @@
         </div>
     </div>
 </div>
-{{--<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>--}}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </body>
 </html>
